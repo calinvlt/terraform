353c45ee31d03b042ed0954075e1998ce1250ddb
Calin V <calinv@live.com>
2020-05-01 10:31:10 -0400

    Added web server and SG

--- first-instance/main.tf
+++ first-instance/main.tf
@@ -3,11 +3,28 @@ provider "aws" {
 }
 
 resource "aws_instance" "firstinstance" {
-    ami             = "ami-0323c3dd2da7fb37d"
+    ami             = "ami-085925f297f89fce1"
     instance_type   = "t2.micro"
+
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello World!" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
 
     tags = {
         Name = "First Instance"
+    }
+}
+
+resource "aws_security_group" "instance_sg" {
+    name        = "firstinstance_sg"
+
+    ingress {
+        from_port   = 8080
+        to_port     = 8080
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
     }
 }
 
